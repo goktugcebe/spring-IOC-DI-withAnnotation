1d879de67e7c93369af7657ee75ddf49556a9854
goktu <goktugcebe@gmail.com>
2023-01-28 17:34:22 +0100

    Initial commit

---
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class TennisCoach implements Coach {
+
+    private final FortuneService fortuneService;
+
+    @Autowired
+    public TennisCoach(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+
+}
